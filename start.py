a6ffe9f26a271adb1df52c8b974e57451f0c200c
biadar1 <bartoszbia79@gmail.com>
2020-02-29 13:44:18 +0100

    koniec 1 cwiczen

---
+++ start.py
@@ -0,0 +1,14 @@
+print("Hello world") #wersja 3.x
+# print "Hello world" wersja 2.x
+# def main():
+#     for elem in kolekcja:
+#         if costam <0:
+imie="abc"
+imie=0
+imie= None
+
+#łańcuchy znaków
+imie = "Ala"
+print(type(imie))
+<class 'str'>
+
